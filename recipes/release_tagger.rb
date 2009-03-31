c424fffe9745f78cf3d5fafa4adb91a897bbbdf3
Jeff Dean <jeff@zilkey.com>
2009-03-31 01:01:35 -0400

    Added write_to_shared and read_from_shared tasks

--- recipes/release_tagger.rb
+++ recipes/release_tagger.rb
@@ -15,7 +15,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       end
     end
 
-    task :latest_tags do
+    task :print_latest_tags, :roles => :app do
       logger.info "AUTOTAG release tag history is:"
       entries = CapistranoHelper.new(variables).release_tag_entries
       entries.each do |entry|
@@ -23,11 +23,20 @@ Capistrano::Configuration.instance(:must_exist).load do
       end
     end
 
+    task :write_tag_to_shared, :roles => :app do
+      run "echo '#{branch}' > #{shared_path}/released_git_tag.txt"
+    end
+
+    task :read_tag_from_shared, :roles => :app do
+      run "cat #{shared_path}/released_git_tag.txt"
+    end
+
     desc %Q{Creates a tag using the current_stage variable}
-    task :create_tag do
+    task :create_tag, :roles => :app do
       if variables[:current_stage]
-        tag_name = AutoTagger.new(variables[:current_stage], variables[:working_directory]).create_tag
-        logger.info "created and pushed tag #{tag_name}"
+        previous_tag = AutoTagger.new(StageManager.new(stages).previous_stage(current_stage), Dir.pwd).latest_tag
+        tag_name = AutoTagger.new(variables[:current_stage], variables[:working_directory]).create_tag(previous_tag)
+        logger.info "AUTO TAGGER created tag #{tag_name} from #{previous_tag.inspect}"
       else
         logger.info "AUTO TAGGER WARNING: skipping auto-creation of tag.  Please specify :current_stage to enable auto-creation of tags (like set :current_stage, :ci)."
       end
