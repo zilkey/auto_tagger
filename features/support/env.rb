be4f6274d79c71a8f7ed4aee6932670576024d24
Jeff Dean <jeff@zilkey.com>
2010-08-05 21:09:34 -0400

    removed activesupport dependency for running tests, removed geminstaller config

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,12 @@
 require 'spec'
 require 'erb'
 require 'etc'
-require 'activesupport'
+
+class String
+  def starts_with?(prefix)
+    self[0, prefix.length] == prefix
+  end
+end
 
 Before do
   StepHelpers.new.reset
